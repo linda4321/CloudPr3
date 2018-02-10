e47885ffb015db8275a556d0adeb766d76da7681
IloveCoffeeAndAnime <ksenia.magur@gmail.com>
2018-02-10 19:35:40 +0200

    Add add function

--- first_project/src/first_project/Class.java
+++ first_project/src/first_project/Class.java
@@ -3,7 +3,10 @@ package first_project;
 public class Class {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println(add(4, 7));
 	}
 
+	private int add(int a, int b){
+		return a+b;
+	}
 }
