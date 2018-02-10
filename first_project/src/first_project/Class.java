0b16e3817181ad28c3b6b2c269ac0a686ad82c7f
Polina <magur.polina@gmail.com>
2018-02-10 19:40:25 +0200

    Add function

--- first_project/src/first_project/Class.java
+++ first_project/src/first_project/Class.java
@@ -3,7 +3,11 @@ package first_project;
 public class Class {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println(add(8, 9));
+	}
+	
+	private int add(int a, int b){
+		return a-b;
 	}
 
 }
